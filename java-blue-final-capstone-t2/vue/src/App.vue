1084263ef8926769a117e5beb6b4f5b6cea3bb02
Georgia Angelakis <spcemonky2@gmail.com>
2021-02-12 15:17:39 -0500

    capstone

---
+++ java-blue-final-capstone-t2/vue/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      
+      <router-link v-bind:to="{ name: 'home' }" v-if="$store.state.token != ''"><button class = "home-button">Home</button></router-link> &nbsp;&nbsp;
+      <router-link v-bind:to="{name: 'matched'}" v-if="$store.state.token != ''"><button class = "liked-button">Liked Restaurants</button></router-link>&nbsp;&nbsp;
+      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''"><button class = "logout-button">Logout</button></router-link> 
+      
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style>
+@import './assets/Main.css';
+
+
+.logout-button {
+  background-color: #AFEADC;
+  border-color: #041E42;
+  border-radius: 5px;
+  height: 25px;
+  width: 75px;
+  font-family: Quicksand;
+  font-weight: bold;
+  text-transform: uppercase;
+}
+
+.liked-button {
+  background-color: #AFEADC;
+  border-color: #041E42;
+  border-radius: 5px;
+  height: 25px;
+  width: 155px;
+  font-family: Quicksand;
+  font-weight: bold;
+  text-transform: uppercase;
+}
+
+.home-button {
+  background-color: #AFEADC;
+  border-color: #041E42;
+  border-radius: 5px;
+  height: 25px;
+  width: 75px;
+  font-family: Quicksand;
+  font-weight: bold;
+  text-transform: uppercase;
+}
+
+
+</style>
